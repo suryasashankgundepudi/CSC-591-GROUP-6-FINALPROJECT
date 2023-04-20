791b172bf431039f09f42b2f14494675c3712a8b
PranaviSan <46625997+PranaviSan@users.noreply.github.com>
2023-04-19 22:09:11 -0400

    Updated xpln1 and xpln2 methods

--- src/misc.py
+++ src/misc.py
@@ -1,3 +1,4 @@
+import json
 import sys, re, copy
 from numerics import *
 from config import *
--- src/xpln.py
+++ src/xpln.py
@@ -20,6 +20,26 @@ class XPLN:
         rule,most=self.firstN(sorted(tmp,key = lambda x: x['val'],reverse=True),self.score)
         return rule,most  
     
+    def xpln2(self, data, best, rest):
+        def v(has):
+            return value(has, len(best.rows), len(rest.rows), 'best')
+        def score(self, ranges):
+            rule = self.RULE(ranges, self.maxSizes)
+            if rule:
+                bestr = selects2(rule, self.best.rows)
+                restr = selects2(rule, self.rest.rows)
+                if len(bestr) + len(restr) > 0:
+                    return value({'best': len(bestr), 'rest': len(restr)}, len(self.best.rows), len(self.rest.rows), 'best'), rule
+        return None,None
+        
+        tmp,self.maxSizes = [],{}
+        for _,ranges in enumerate(bins(data.cols.x,{'best':best.rows, 'rest':rest.rows})):
+            self.maxSizes[ranges[0].txt] = len(ranges)
+            for _,range in enumerate(ranges):
+                tmp.append({'range':range, 'max':len(ranges),'val': v(range.y.has)})
+        rule,most=self.firstN2(sorted(tmp,key = lambda x: x['val'],reverse=True),self.score)
+        return rule,most 
+    
     def firstN(self, sorted_ranges, scoreFun):
         first = sorted_ranges[0]['val']
 
@@ -29,6 +49,28 @@ class XPLN:
         sorted_ranges = [s for s in sorted_ranges if useful(s)]
         most = -1
         out = -1
+        for n in range(len(sorted_ranges)):
+            tmp, rule = scoreFun([r['range'] for r in sorted_ranges[:n+1]])
+            if tmp is not None and tmp > most:
+                out, most = rule, tmp
+        return out, most
+    
+    def firstN2(self, sorted_ranges, scoreFun):
+        first = sorted_ranges[0]['val']
+
+        def useful(range):
+            if range['val'] > 0.05 and range['val'] > first / 10:
+                return range
+            
+        def neg(range):
+            if range['val'] < 0.05 and range['val'] < first / 10:
+                return range
+
+        negranges = list(filter(neg, sortedRanges))
+        negranges.reverse()
+        sortedRanges = list(filter(useful, sortedRanges))
+        sorted_ranges = [s for s in sorted_ranges if useful(s)]
+        most,out = -1,-1
         for n in range(len(sorted_ranges)):
             tmp, rule = scoreFun([r['range'] for r in sorted_ranges[:n+1]])
             if tmp is not None and tmp > most:
@@ -48,15 +90,6 @@ class XPLN:
         if n > 0: 
             return new_rule
         return None
-    
-    def score(self, ranges):
-        rule = self.RULE(ranges, self.maxSizes)
-        if rule:
-            bestr = selects(rule, self.best.rows)
-            restr = selects(rule, self.rest.rows)
-            if len(bestr) + len(restr) > 0:
-                return value({'best': len(bestr), 'rest': len(restr)}, len(self.best.rows), len(self.rest.rows), 'best'), rule
-        return None,None
     
     def RULE(self, ranges,maxSize):
         t={}
@@ -100,6 +133,43 @@ def selects(rule, rows):
             if not disjunction(ranges, row):
                 return False
         return True
+
+    def function(r):
+        return r if conjunction(r) else None
+    
+    r = []
+    for item in list(map(function, rows)):
+        if item:
+            r.append(item)
+    return r
+
+def selects2(rule, rows):
+    def disjunction(ranges, row):
+        for rang in ranges:
+            at = rang['at']
+            x = row.cells[at]
+            lo = rang['lo']
+            hi = rang['hi']  
+            if x == '?' or (lo == hi and lo == x) or (float(lo) <= float(x) and float(x)< float(hi)):
+                return True
+            if x.replace(".", "").isnumeric():
+                x = float(x) 
+            else:
+                x
+        return False
+
+    def conjunction(row):
+        if rule:
+            for ranges in rule['pos'].values():
+                for neg in rule['neg'].values():
+                    if disjunction(neg, row):
+                        return False
+                if not disjunction(ranges, row):
+                    return False
+            return True
+        else:
+            return True
+
 
     def function(r):
         return r if conjunction(r) else None
